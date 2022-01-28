6fa21778f08320e4967965f774694802f378b975
DiegoElNegro <diego.g729@slcstudents.org>
2022-01-28 15:01:31 -0700

    added colors and emojis

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["black", "red", "blue", "purple", "brown", "pink", "green", "yellow", "green", "grey", "white", "teal", "aqua", "gold", "orange", "lime"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
         const randomNumber =  getRandomNumber();
-        console.log(randomNumber);
-    
+        //console.log(randomNumber);
+        document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
 });
 
 function getRandomNumber(){
